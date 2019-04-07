9781da21827c72b692315f93568c0a47ded1a80d
Mohsin Nazir <mohsin2851@outlook.com>
2019-04-07 18:19:25 +0500

    Added

---
+++ myAngularApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+let num = 54;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myAngularApp which is your first app';
+}
