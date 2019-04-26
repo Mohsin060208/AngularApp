2a57bbd82e6354a392bc672e56f79a92881195d2
Mohsin Nazir <mohsin2851@outlook.com>
2019-04-26 16:58:44 +0500

    .

--- myAngularApp/src/app/login/login.component.ts
+++ myAngularApp/src/app/login/login.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  email = "abc@outlook.com";
-  pass = "12439";
-  submitted = false;
-  onSubmit() { this.submitted = true; }
-}
+    constructor() { 
+      
+    }
+    ngOnInit() {
+      
+    }
+  
+  }
