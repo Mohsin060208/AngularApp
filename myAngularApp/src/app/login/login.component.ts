c1a72a75f6304d6d86db24dd73d091dc8a2e0b78
Mohsin Nazir <mohsin2851@outlook.com>
2019-04-29 14:52:41 +0500

    Login

--- myAngularApp/src/app/login/login.component.ts
+++ myAngularApp/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,11 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-    constructor() { 
-      
+    constructor(private router:Router) { 
     }
     ngOnInit() {
-      
     }
-  
+    submitted = false;
+    pass : string;
+    PassCheck = "Enter registered Password";
+    email: string;
+    EmailCheck = "Enter registered Email";
+    onSubmit() { 
+      if(this.submitted == true){
+        this.router.navigate(['/about']);
+      }
+      else{
+        alert("Please fill in the form with valid credentials.");
+        this.router.navigate(['/login']);
+      }
+    }
+    checkPassword(event: Event){
+      this.pass = (<HTMLInputElement>event.target).value.toString();
+      console.log(this.pass);
+    if(this.pass == "12439"){
+      this.PassCheck = "Password is correct";
+      this.submitted = true; 
+    }
+    else{
+      this.PassCheck = "Password is Incorrect";
+      this.submitted = false;
+      }
+    }
+    checkEmail(event: Event){
+      this.email = (<HTMLInputElement>event.target).value.toString();
+      console.log(this.email);
+    if(this.email == "123@outlook.com"){
+      this.EmailCheck = "Email is correct";
+      this.submitted = true; 
+    }
+    else{
+      this.EmailCheck = "Email is Incorrect";
+      this.submitted = false;
+      }
+    }
   }
