39b014138b19ee0338a2d6b681977484820a400d
Mohsin Nazir <mohsin2851@outlook.com>
2019-05-03 12:11:56 +0500

    till Dashboard

--- myAngularApp/src/app/app.module.ts
+++ myAngularApp/src/app/app.module.ts
@@ -7,12 +7,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from '../auth.guard';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -20,12 +15,6 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     DashboardComponent,
     LoginComponent,
     HomeComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,    
